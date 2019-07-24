5f3dd0c946c60dae3e5c7adc6fccc0c5f1d11963
aminaanaan <amina-mo1@hotmail.com>
2019-07-24 16:47:41 +0200

    login

--- src/test/java/com/techprimers/security/securitydbexample/SecurityDbExampleApplicationTests.java
+++ src/test/java/com/techprimers/security/securitydbexample/SecurityDbExampleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.bot;
+package com.techprimers.security.securitydbexample;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WelcomeBotApplicationTests {
+public class SecurityDbExampleApplicationTests {
 
 	@Test
 	public void contextLoads() {
