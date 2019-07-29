b8369c089b509e4f432d1dc147269103dcc55875
Dimness <prusina1504@gmail.com>
2019-07-29 14:42:05 +0200

    Re-commit

---
+++ src/main/java/slack/api/model/messagepayloadmodel/PayloadBlockText.java
@@ -0,0 +1,36 @@
+package slack.api.model.messagepayloadmodel;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+"type",
+"text"
+})
+public class PayloadBlockText {
+
+	// Properties
+	@JsonProperty("type")
+	private String type;
+	@JsonProperty("text")
+	private String text;
+	
+	// Constructor
+	public PayloadBlockText() {}
+	
+	// Getters/Setters
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+}
