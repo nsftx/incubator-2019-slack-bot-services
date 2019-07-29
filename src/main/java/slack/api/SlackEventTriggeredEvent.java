23e376834464bc378bd33e0a511399f6e0575d5d
Dimness <prusina1504@gmail.com>
2019-07-29 14:52:23 +0200

    Re-commit of Slack Controller, Slack Event Service, Slack Command Service, Slack Event Triggered Event classes

---
+++ src/main/java/slack/api/SlackEventTriggeredEvent.java
@@ -0,0 +1,20 @@
+package slack.api;
+
+import java.util.HashMap;
+
+import org.springframework.context.ApplicationEvent;
+
+@SuppressWarnings("serial")
+public class SlackEventTriggeredEvent extends ApplicationEvent {
+
+	private HashMap<String,String> eventData = new HashMap<String,String>();
+	
+	public SlackEventTriggeredEvent(Object source, HashMap<String,String> eventData) {
+		super(source);
+		this.eventData = eventData;
+	}
+	
+	public HashMap<String,String> getEventData(){
+		return eventData;
+	}
+}
