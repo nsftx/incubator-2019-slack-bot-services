59ab7a6375d516a82cf74857d0e2bdec2500610a
Amina Fajić <37187803+aminaanaan@users.noreply.github.com>
2019-08-13 22:43:57 +0200

    Update BaseException.java

--- src/main/java/com/welcome/bot/exception/base/BaseException.java
+++ src/main/java/com/welcome/bot/exception/base/BaseException.java
@@ -3,52 +3,16 @@ package com.welcome.bot.exception.base;
 import java.util.Date;
 
 import org.springframework.http.HttpStatus;
-
+@SuppressWarnings("serial")
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class BaseException extends RuntimeException {
+ public BaseException() {
+        
+    }
+	
+    public BaseException(String message) {
+        super(message);
+    }
 
-	private Date timestamp;
-	private HttpStatus status;
-	private Integer error;
-	private String path;
 
-	public BaseException(String message, Throwable cause) {
-		super(message, cause);
-		this.timestamp = new Date();
-	}
-
-	public BaseException(String message) {
-		super(message);
-		this.timestamp = new Date();
-	}
-
-	public BaseException(String message, HttpStatus status) {
-		super(message);
-		this.timestamp = new Date();
-		this.status = status;
-		this.error = status.value();
-	}
-
-	public BaseException(String message, HttpStatus status, String path) {
-		super(message);
-		this.timestamp = new Date();
-		this.status = status;
-		this.error = status.value();
-		this.path = path;
-	}
-
-	public Date getTimestamp() {
-		return timestamp;
-	}
-
-	public HttpStatus getStatus() {
-		return status;
-	}
-
-	public String getPath() {
-		return path;
-	}
-
-	public Integer getError() {
-		return this.error;
-	}
 }
