9c8c686dc3a5dba5f7b1d9b4f073e4b967e55c1e
Amina Fajić <37187803+aminaanaan@users.noreply.github.com>
2019-08-14 13:21:41 +0200

    Update User.java

--- src/main/java/com/welcome/bot/domain/User.java
+++ src/main/java/com/welcome/bot/domain/User.java
@@ -5,6 +5,7 @@ import com.welcome.bot.models.AuthProvider;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "user", uniqueConstraints = {
