bbea4be4aa1979b97907352740e1cf498502b011
Amina Fajić <37187803+aminaanaan@users.noreply.github.com>
2019-08-14 13:44:48 +0200

    Update CustomOAuth2UserService.java

--- src/main/java/com/welcome/bot/security/oauth2/CustomOAuth2UserService.java
+++ src/main/java/com/welcome/bot/security/oauth2/CustomOAuth2UserService.java
@@ -58,7 +58,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     }
     
     private User updateExistingUser(User existingUser, OAuth2UserInfo oAuth2UserInfo) {
-        existingUser.setName(oAuth2UserInfo.getName());
+        //existingUser.setName(oAuth2UserInfo.getName());
         existingUser.setImageUrl(oAuth2UserInfo.getImageUrl());
         return userRepository.save(existingUser);
     }
