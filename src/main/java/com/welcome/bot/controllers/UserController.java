80bb3244822205b8d11ec71b1dd66ad0ade1709b
Amina Fajić <37187803+aminaanaan@users.noreply.github.com>
2019-08-18 16:46:41 +0200

    Update UserController.java

--- src/main/java/com/welcome/bot/controllers/UserController.java
+++ src/main/java/com/welcome/bot/controllers/UserController.java
@@ -74,12 +74,28 @@ public class UserController {
     public TranslationSettings translate() {
  
     	TranslationSettings data=new TranslationSettings();
-    	data.setTitle(messageSource.getMessage("settings", null, LocaleContextHolder.getLocale()));
+    	data.setSettings(messageSource.getMessage("settings", null, LocaleContextHolder.getLocale()));
     	data.setTheme(messageSource.getMessage("theme", null, LocaleContextHolder.getLocale()));
     	data.setSelectColor(messageSource.getMessage("select.color", null, LocaleContextHolder.getLocale()));
     	data.setSelectLanguage(messageSource.getMessage("select.language", null, LocaleContextHolder.getLocale()));
     	data.setLanguage(messageSource.getMessage("language", null, LocaleContextHolder.getLocale()));
     	data.setSave(messageSource.getMessage("save", null, LocaleContextHolder.getLocale()));
+         data.setChannel(messageSource.getMessage("channel",null,LocaleContextHolder.getLocale()));
+		data.setMessages(messageSource.getMessage("messages",null,LocaleContextHolder.getLocale()));
+		data.setMessage(messageSource.getMessage("message",null,LocaleContextHolder.getLocale()));
+		data.setSchedules(messageSource.getMessage("schedules",null,LocaleContextHolder.getLocale()));
+		data.setUsers(messageSource.getMessage("users",null,LocaleContextHolder.getLocale()));
+		data.setActiveAt(messageSource.getMessage("activeAt",null,LocaleContextHolder.getLocale()));
+		data.setTriggers(messageSource.getMessage("triggers",null,LocaleContextHolder.getLocale()));
+		data.setTrigger(messageSource.getMessage("trigger",null,LocaleContextHolder.getLocale()));
+		data.setName(messageSource.getMessage("name",null,LocaleContextHolder.getLocale()));
+		data.setRole(messageSource.getMessage("role",null,LocaleContextHolder.getLocale()));
+		data.setNextRun(messageSource.getMessage("nextRun",null,LocaleContextHolder.getLocale()));
+		data.setTitle(messageSource.getMessage("title",null,LocaleContextHolder.getLocale()));
+		data.setText(messageSource.getMessage("text",null,LocaleContextHolder.getLocale()));
+		data.setActive(messageSource.getMessage("active",null,LocaleContextHolder.getLocale()));
+		data.setRepeat(messageSource.getMessage("active",null,LocaleContextHolder.getLocale()));
+
      return data;
     }
     
